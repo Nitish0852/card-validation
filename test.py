ba53fb326791e470c2316321bf17feb7f6ed20aa
Nitish <43701468+Nitish0852@users.noreply.github.com>
2021-02-03 18:15:32 +0530

    unit testing

---
+++ test.py
@@ -0,0 +1,17 @@
+import luhn
+
+def valid():
+	tests = [
+		("0000000000000000", True),
+		("4111111111111111", True),
+		("5500000000000004", True),
+		("6250941006528599", True),
+		("5425233430109903", True),
+		("6034883265619896", True),
+		("4242424242424249", False),
+		]
+
+	for t in tests:
+		num, res = t
+		assert luhn.verify(num)
+
